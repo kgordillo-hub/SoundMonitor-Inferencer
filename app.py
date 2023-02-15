40b0181a620ea3e7454e1e962d91ad9201184936
kgordillo-hub <ksgordilloo@correo.udistrital.edu.co>
2023-02-15 15:08:08 -0500

    formatting code

--- app.py
+++ app.py
@@ -9,17 +9,13 @@ import json
 import boto3
 import logging
 
-
 from inferencer.adapa_task5 import DcaseAdapatask5
 import soundfile as sf
 import io
 import time
-import datetime
-
 
 import flask
 from flask import Response, request
-
 
 application = flask.Flask(__name__)
 application.config.from_object("default_config")
@@ -30,86 +26,94 @@ s3 = boto3.client("s3")
 
 @application.route("/identifySound", methods=["POST"])
 def identify_sound():
-    
     start_time = time.time()
-    
-    response = None
-    
+
     if request.json is None:
         # Expect application/json request
         response = Response("Empty request", status=415)
     else:
         request_content = json.loads(request.data)
-        message = dict()
         try:
             content_json = json.loads(request_content["Message"])
-
+            #
             if request_content["TopicArn"] and request_content["Message"]:
                 message = content_json["Records"][0]["s3"]
             else:
                 message = request_content
-                
+
             bucket = message["bucket"]["name"]
             archivo = message["object"]["key"]
-            
-            tmp_prnt = "Descargar el archivo: "+ archivo + " del bucket: " + bucket
-            
-            out_file_path='/tmp/' + str(archivo)
+
+            tmp_prnt = "Descargar el archivo: " + archivo + " del bucket: " + bucket
+
+            out_file_path = '/tmp/' + str(archivo)
             logging.warn(tmp_prnt)
 
-            s3.download_file(Bucket = bucket, Key = archivo, Filename = out_file_path)
-            
+            s3.download_file(Bucket=bucket, Key=archivo, Filename=out_file_path)
+
             logging.warn("Inicializa el inferenciador")
-               
+
             inferencer = DcaseAdapatask5()
-            
-            
+
             with open(out_file_path, 'rb') as audio:
                 data, samplerate = sf.read(io.BytesIO(audio.read()))
-            
-            result = inferencer.run_inferencer(archivo, data, samplerate)
+
+            result = inferencer.run_inferencer(str(archivo), data[:, 0], samplerate)
             result_json = result.to_json()
             logging.warn(result_json)
             logging.warn("Finaliza el inferenciador")
-            
-            
-            #sns_client = boto3.client("sns", region_name='us-east-1')
-            
+
             processing_time = (time.time() - start_time)
-            
-            service_response = {'ProcessingTime_seconds' : processing_time, 'Date': str(datetime.datetime.now()), 'Inferencer_result' : json.loads(result_json)}
-            
+            file_date = get_file_date(str(archivo))
+            file_hour = get_file_hour(str(archivo))
+
+            service_response = {'ProcessingTime_seconds': processing_time,
+                                'Audio_fecha': file_date,
+                                'Audio_hora': file_hour,
+                                'Inferencer_result': json.loads(result_json)}
+
             logging.warn("Service response to send SNS: %s" % service_response)
-            
-            
+
             s3.put_object(
-                Body=(bytes(json.dumps(service_response).encode('UTF-8'))), 
-                Bucket = 'soundmonitor-noise-type', 
-                Key = 'NoiseType_'+str(archivo)+'-'+str(time.time())+'_.json'
-			)
+                Body=(bytes(json.dumps(service_response).encode('UTF-8'))),
+                Bucket='soundmonitor-noise-type',
+                Key='NoiseType_' + str(archivo) + '-' + str(time.time()) + '_.json'
+            )
+
             response = Response("", status=200)
-            #response = sns_client.publish(
-            #TopicArn = "arn:aws:sns:us-east-1:703106094997:audio_result_event",
-            #Message = json.dumps({'statusCode': 200, 'body': service_response})
-            #)
-            
-            #logging.warn("Response from SNS")
-            #logging.warn(response)
         except Exception as ex:
             sns_client = boto3.client("sns", region_name='us-east-1')
             response = sns_client.publish(
-               TopicArn = "arn:aws:sns:us-east-1:703106094997:audio_error_topic",
-               Message = json.dumps({'statusCode': 500, 'error_processing': request.json, 'exception': str(ex.args[0])})
+                TopicArn="arn:aws:sns:us-east-1:703106094997:audio_error_topic",
+                Message=json.dumps({'statusCode': 500, 'error_processing': request.json, 'exception': str(ex.args[0])})
             )
             logging.exception("Error processing message: %s" % request.json)
             logging.exception(ex)
-    
+
     return response
+
+
+def get_file_date(file_name):
+    date_split = file_name.split("_")
+    if len(date_split) > 1:
+        return date_split[1] + ""
+    else:
+        return ""
+
+
+def get_file_hour(file_name):
+    date_split = file_name.split("_")
+    if len(date_split) > 2:
+        return date_split[2] + ""
+    else:
+        return ""
+
 
 @application.route("/")
 def print_hello():
     response = Response("Hello", status=200)
     return response
+
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0")
